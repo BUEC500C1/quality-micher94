093f8748cbdbc27a4b1d8dfd6c18d25a263bf9c7
micher94 <55001134+micher94@users.noreply.github.com>
2020-02-03 17:53:54 -0500

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -3,12 +3,11 @@ import pytest
 
 #don't know why the pytest is showing up as no tests run
 
-def main():
+def test_values():
 	#convert_numeral(2150)
 	print("hello world")
 	# assert convert_numeral(2150) == "MMCL"
 	#convert_numeral(10123)
 	#convert_numeral(-1023)
 	#convert_numeral(1.2)
-if __name__ == '__main__':
-    main()
+
