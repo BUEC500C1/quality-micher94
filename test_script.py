9f503f177a99caf3e8ee9ade995b1cf8f8f2ced1
micher94 <55001134+micher94@users.noreply.github.com>
2020-02-03 17:39:32 -0500

    Trying a to test with a different file name

---
+++ test_script.py
@@ -0,0 +1,11 @@
+from Converter.py import convert_numeral 
+import pytest
+
+#don't know why the pytest is showing up as no tests run
+
+def first_test():
+	#convert_numeral(2150)
+	assert convert_numeral(2150) == "MMCL"
+	#convert_numeral(10123)
+	#convert_numeral(-1023)
+	#convert_numeral(1.2)
