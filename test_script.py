c47cde3647dce90f52696873fccc35e46411210c
micher94 <55001134+micher94@users.noreply.github.com>
2020-02-03 20:48:50 -0500

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -6,7 +6,7 @@ import pytest
 def test_values():
 	#convert_numeral(2150)
 	assert convert_numeral(2150) == "MMCL"
-	assert convert_numeral(10123)
-	assert convert_numeral(-1023)
-	assert convert_numeral(1.2)
+	assert convert_numeral(2123) == ""MMCXXIII"
+	assert convert_numeral(-1023) == 0
+	assert convert_numeral(1.2) == 0
 
