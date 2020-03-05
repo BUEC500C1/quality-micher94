b0aa1ba25ff982c6c115810418694f850528bb98
micher94 <55001134+micher94@users.noreply.github.com>
2020-03-04 19:59:21 -0500

    Update test_script.py
    Added additional unit tests and test for non-numeric characters

--- test_script.py
+++ test_script.py
@@ -3,8 +3,13 @@ import pytest
 
 def test_values():
 	#convert_numeral(2150)
+	
+	assert convert_numeral(13) == "XIII"
+	assert convert_numeral(163) == "CLXIII"
+	assert convert_numeral(862) == "DCCCLXII"
 	assert convert_numeral(2150) == "MMCL"
 	assert convert_numeral(2123) == "MMCXXIII"
 	assert convert_numeral(-1023) == False
 	assert convert_numeral(1.2) == False
+	assert convert_numeral('word') == False
 
